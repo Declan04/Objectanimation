39bde8ed5f69b70438bc9bce6c71745d0171a5dc
20686 <20686@portlandschools.org>
2021-10-05 09:33:54 -0400

    finshied and drew the beach.

--- object oriented Animation.py
+++ object oriented Animation.py
@@ -9,31 +9,34 @@ GREEN = (12, 148, 37)
 BLUE = (2, 0, 94)
 BLACK = (0, 0, 0)
 PURPLE = (28, 4, 54)
-
+SAND_YELLOW = (76, 70, 50)
 
 #width by height
 FPS = 60
 DISPLAYWIDTH = 700
 DISPLAYHEIGHT = 500
 pygame.init()
+screen = pygame.display.set_mode((DISPLAYWIDTH, DISPLAYHEIGHT))
 
-class beach():
+class Beach():
 
-    def __init__(self,x,y,width,height,color1,color2,screen):
+    def __init__(self,x,y,width,height,color1,screen):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.color1 = color1
+        self.screen = screen
+
+    def draw_beach(self):
+        pygame.draw.rect(self.screen,self.color1,[self.x,self.y,self.width,self.height])
 
 
 
 
 
 
-
-
-
-screen = pygame.display.set_mode((DISPLAYWIDTH, DISPLAYHEIGHT))
+beach = Beach(0,300,900,600,SAND_YELLOW,screen)
 
 pygame.display.set_caption("my Pygame animation")
 
@@ -48,10 +51,10 @@ while running:
 
         if event.type == pygame.QUIT:
             running = False
-    screen.fill(PURPLE)
+    screen.fill(BLUE)
 
 
-
+    beach.draw_beach()
 
     pygame.display.flip()
 
